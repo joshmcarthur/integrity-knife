6440bd0a5db27e715ed697418a2a6d552f89e029
Josh McArthur <joshua.mcarthur@gmail.com>
2013-06-15 23:28:26 +1200

    Working chef run

--- site-cookbooks/integrity/recipes/default.rb
+++ site-cookbooks/integrity/recipes/default.rb
@@ -1,5 +1,5 @@
 # Packages first
-%w(memcached redis-server htop fail2ban git-core ).each do |required_package|
+%w( memcached redis-server htop fail2ban git-core ).each do |required_package|
   package required_package do
     action :install
   end
@@ -12,25 +12,19 @@ include_recipe 'unattended-upgrades'
 include_recipe 'postgresql::server'
 include_recipe 'postgresql::client'
 include_recipe 'sqlite'
-include_recipe 'runit'
+include_recipe 'ruby_build'
+include_recipe 'rbenv::system'
+# include_recipe 'rbenv_passenger::nginx'
 
-include_recipe 'rvm::system_install'
-
-rubies = ['1.8.7', '1.9.2', '2.0.0-p195', '1.9.3']
+rubies = ['1.8.7-p371', '1.9.2-p320', '2.0.0-p195', '1.9.3-p429']
 rubies.each do |ruby|
-  rvm_ruby ruby
+  rbenv_ruby ruby
 end
 
 # Set default ruby to 1.9.3
 # TODO update to 2.0 once Chef works on Ruby 2
 # Current error looks like: cannot load such file -- rubygems/format
-rvm_default_ruby rubies.last
-
-# node.override['firewall']['rules'] = [
-#   {"http" => {"port" => 80}},
-#   {"https" => {"port" => 443}},
-#   {"ssh" => {"port" => 22}}
-# ]
+rbenv_global rubies.last
 
 firewall "ufw" do
   action :enable
@@ -62,28 +56,106 @@ user_account 'integrity' do
   ssh_keygen false
 end
 
-# Ensure unicorn is installed
-rvm_gem "unicorn" do
-  ruby_string rubies.last
-  action :install
-end
 
 # Ensure chef will be present for future chef runs
-rvm_gem "chef" do
-  ruby_string rubies.last
+rbenv_gem "chef" do
+  rbenv_version rubies.last
   action :install
 end
 
+# Ensure bundler is present
+rbenv_gem "bundler" do
+  rbenv_version rubies.last
+  action :install
+end
+
+rbenv_gem "passenger" do
+  rbenv_version rubies.last
+  action :install
+end
+
+rbenv_rehash "Rehash installed gems"
+
+# Nginx/Passenger dependencies
+%w( nginx-common libcurl4-openssl-dev libssl-dev zlib1g-dev libpcre3-dev ).each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+# Remove Nginx in case it's installed
+%w( nginx ).each do |unrequired_pkg|
+  package unrequired_pkg do
+    action :purge
+  end
+end
+
+rbenv_script "Compile Nginx with Passenger support" do
+  rbenv_version rubies.last
+  user 'root'
+  group 'root'
+  timeout 5000
+  returns [0, 2]
+  code %{ passenger-install-nginx-module --auto --auto-download --prefix=/opt/nginx }
+  not_if do
+    File.directory?("/opt/nginx")
+  end
+end
+
+# Add our own Nginx config that isn't so loaded with
+# commented-out stuff and is ready to serve sites.
+template "/opt/nginx/conf/nginx.conf" do
+  source "nginx.conf.erb"
+  mode 0600
+  owner "root"
+  group "root"
+end
+
+directory "/opt/nginx/sites-available" do
+  action :create
+end
+
+directory "/opt/nginx/sites-enabled" do
+  action :create
+end
+
+template "/opt/nginx/sites-available/integrity.conf" do
+  source "integrity.conf.erb"
+  mode 0600
+end
+
+link "/opt/nginx/sites-enabled/integrity.conf" do
+  to "/opt/nginx/sites-available/integrity.conf"
+end
 
 application 'integrity' do
   owner 'integrity'
   group 'integrity'
   path '/home/integrity/apps/integrity'
   repository 'git://github.com/3months/integrity.git'
+  purge_before_symlink ["db"]
+  symlinks({"db" => "db"})
+  migrate true
+  migration_command "bundle exec rake db"
+  before_migrate do
+    rbenv_script "Bundle install" do
+      rbenv_version rubies.last
+      cwd release_path
+      user 'integrity'
+      group 'integrity'
+      code %{ bundle install --deployment --without test --path /home/integrity/apps/integrity/shared/bundle }
+    end
+  end
+
   revision 'master'
+end
 
-
-  passenger_apache2 do
+# Create shared directories unless they already exist
+%w( bundle db ).each do |shared_dir|
+  directory "/home/integrity/apps/integrity/shared/#{shared_dir}" do
+    owner "integrity"
+    group "integrity"
+    action :create
   end
 end
 
